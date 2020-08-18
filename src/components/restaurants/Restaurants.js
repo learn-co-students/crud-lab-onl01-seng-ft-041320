b68862055ff655656daf093f6adfa273627c95d2
mattenbar <matthw.enbar@gmail.com>
2020-08-18 18:21:00 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
+  
+  
   render() {
+    const restaurants = props.restaurants.map(restaurant => <Restaurant key={restaurant.id} {...restaurant}/>)
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -4,4 +4,15 @@ export const cuidFn = cuid;
 
 export default function manageRestaurants(state, action) {
 
+  switch (action.type){
+    case 'ADD_RESTAURANT':
+      
+      const restaurant = {id: cuid(), text: action.text}
+
+      return {...state, restaurants: [...state.restaurants, restaurant]}
+
+    default:
+      return state;
+  }
+
 }
