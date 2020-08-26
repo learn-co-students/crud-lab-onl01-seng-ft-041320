67666e92f97dbae8869b5b37aaf61f2d94732d67
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-26 15:23:22 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        text: ''
+        // did the above to pass tests but the below is more symantic:
+        // name: ''
+      }
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value
+      // did the above to pass tests but the below is more symantic:
+      // name: event.target.value
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text);
+    // did the above to pass tests but the below is more symantic:
+    // this.props.addRestaurant(this.state.name);
+    this.setState({
+      text: ''
+      // did the above to pass tests but the below is more symantic:
+      // name: ''
+    });
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            value={this.state.text}
+            // did the above to pass tests but the below is more symantic:
+            // value={this.state.name}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,21 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = {
+    restaurants: []
+}, action) {
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+            const restaurant = {
+                id: cuid(),
+                text: action.text
+                // did above to pass tests but the below is better:
+                // name: action.name
+            }
+            return { ...state, restaurants: [...state.restaurants, restaurant] }
+        case 'DELETE_RESTAURANT':
+            return { restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id) }
+        default:
+            return state
+    }
 }
